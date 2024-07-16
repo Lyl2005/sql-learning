4bb576a1b5c2168c488988f6d2057f91ca7c790b
Lyl2005 <lyl_2005@rambler.ru>
2024-07-16 13:34:45 +0300

    Update 1.2.4 Выборка данных по условию.sql

--- 1.2 Выборка данных/1.2.4 Выборка данных по условию.sql
+++ 1.2 Выборка данных/1.2.4 Выборка данных по условию.sql
@@ -1,1 +1,25 @@
+SELECT title, price 
+FROM book
+WHERE price < 600;
 
+SELECT title, author, price * amount AS total
+FROM book
+WHERE price * amount > 4000;
+
+SELECT author,title,price FROM book WHERE amount <10;
+
+SELECT title, author, price 
+FROM book
+WHERE price > 600 AND author = 'Булгаков М.А.';
+
+
+Разные результаты
+SELECT title, author, price 
+FROM book
+WHERE (author = 'Булгаков М.А.' OR author = 'Есенин С.А.') AND price > 600;
+
+и
+
+SELECT title, author, price 
+FROM book
+WHERE author = 'Булгаков М.А.' OR author = 'Есенин С.А.' AND price > 600;
