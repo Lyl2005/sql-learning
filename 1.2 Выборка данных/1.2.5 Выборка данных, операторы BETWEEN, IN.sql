2344348574d001975008ce1b512360482d8a3797
Lyl2005 <lyl_2005@rambler.ru>
2024-07-16 17:45:32 +0300

    Update 1.2.5 Выборка данных, операторы BETWEEN, IN.sql

--- 1.2 Выборка данных/1.2.5 Выборка данных, операторы BETWEEN, IN.sql
+++ 1.2 Выборка данных/1.2.5 Выборка данных, операторы BETWEEN, IN.sql
@@ -11,3 +11,30 @@
 SELECT title, amount 
 FROM book
 WHERE amount BETWEEN 5 AND 14;
+
+SELECT title, amount 
+FROM book
+WHERE amount >= 5 AND amount <=14;
+
+Оператор  IN  позволяет выбрать данные, соответствующие значениям из списка.
+
+Пример
+
+Выбрать названия и цены книг, написанных Булгаковым или Достоевским.
+
+Запрос:
+
+SELECT title, price 
+FROM book
+WHERE author IN ('Булгаков М.А.', 'Достоевский Ф.М.');
+
+То же самое SELECT title, price 
+FROM book
+WHERE author = 'Булгаков М.А.' OR author = 'Достоевский Ф.М.';
+
+Вывести название и авторов тех книг, цены которых принадлежат интервалу от 540.50 до 800 (включая границы), 
+ а количество или 2, или 3, или 5, или 7 .
+ SELECT title, author 
+FROM book
+WHERE price BETWEEN 540.50 AND 800 AND amount IN(2,3,5,7)
+ 
