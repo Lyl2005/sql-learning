79e8b1b67a0fba76e04e8d04ad4202b15a28fd14
Lyl2005 <lyl_2005@rambler.ru>
2024-11-05 22:22:42 +0300

    Update 2.2.8 Запросы на выборку, соединение таблиц.sql

--- 2.2 Запросы на выборку_Соединение таблиц/2.2.8 Запросы на выборку, соединение таблиц.sql
+++ 2.2 Запросы на выборку_Соединение таблиц/2.2.8 Запросы на выборку, соединение таблиц.sql
@@ -1,4 +1,23 @@
+SELECT title,name_author,name_genre,price,amount
+FROM book INNER JOIN author ON author.author_id = book.author_id
+INNER JOIN genre ON  book.genre_id = genre.genre_id
 
+WHERE genre.genre_id IN 
+(SELECT query_in_1.genre_id
+          FROM (SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+               )query_in_1
+            INNER JOIN 
+              (SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+                ORDER BY sum_amount DESC
+                LIMIT 1
+               ) query_in_2
+          ON query_in_1.sum_amount= query_in_2.sum_amount
+         )
+         ORDER BY title
 Сначала определяется таблица, из которой выбираются данные (FROM), 
 затем из этой таблицы отбираются записи в соответствии с условием  WHERE,
 выбранные данные агрегируются (GROUP BY),  из агрегированных записей выбираются те,
