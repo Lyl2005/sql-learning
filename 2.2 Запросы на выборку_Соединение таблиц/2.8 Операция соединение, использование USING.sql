1d8d15f670b663029d419db2b5717a406aef7b7f
Lyl2005 <lyl_2005@rambler.ru>
2024-11-06 14:22:47 +0300

    Update 2.8 Операция соединение, использование USING.sql

--- 2.2 Запросы на выборку_Соединение таблиц/2.8 Операция соединение, использование USING.sql
+++ 2.2 Запросы на выборку_Соединение таблиц/2.8 Операция соединение, использование USING.sql
@@ -1,1 +1,10 @@
-
+Задание
+Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену, 
+  вывести их название и автора, а также посчитать общее количество экземпляров книг в таблицах supply и book,  
+  столбцы назвать Название, Автор  и Количество.
+SELECT book.title AS "Название", name_author AS "Автор",  book.amount + supply.amount AS "Количество"
+FROM 
+    author 
+     INNER JOIN book USING (author_id)
+    INNER JOIN supply ON book.price = supply.price 
+                         and author.name_author = supply.author
