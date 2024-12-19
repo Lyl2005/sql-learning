3effd36df558a9bf50a218c14d65b158bcf6e5b6
Lyl2005 <lyl_2005@rambler.ru>
2024-12-19 18:11:29 +0300

    Update and rename 2.3.1 to 2.3.1 Запросы_на_обновление_связанные_таблицы.sql

---
+++ 2.3 Запросы_корректировки/2.3.1 Запросы_на_обновление_связанные_таблицы.sql
@@ -0,0 +1,10 @@
+Для книг, которые уже есть на складе (в таблице book), но по другой цене, чем в поставке (supply),  необходимо в таблице book увеличить количество на значение, указанное в поставке,  и пересчитать цену. А в таблице  supply обнулить количество этих книг. 
+UPDATE book INNER JOIN author USING(author_id)
+INNER JOIN supply ON book.title = supply.title
+                   AND supply.author = author.name_author
+SET book.amount = book.amount + supply.amount,
+supply.amount = 0,
+book.price = (book.price*book.amount+supply.price*supply.amount)/(book.amount+supply.amount)
+WHERE book.price <> supply.price;
+SELECT * FROM book;
+SELECT * FROM supply
