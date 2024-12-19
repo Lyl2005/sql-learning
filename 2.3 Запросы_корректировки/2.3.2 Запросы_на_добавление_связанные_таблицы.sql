fc2f8f50b70dc6b3db8302538032edd42e5e4c35
Lyl2005 <lyl_2005@rambler.ru>
2024-12-19 18:19:06 +0300

    Update 2.3.2 Запросы_на_добавление_связанные_таблицы.sql

--- 2.3 Запросы_корректировки/2.3.2 Запросы_на_добавление_связанные_таблицы.sql
+++ 2.3 Запросы_корректировки/2.3.2 Запросы_на_добавление_связанные_таблицы.sql
@@ -1,1 +1,13 @@
-
+Запросом на добавление можно добавить записи, отобранные с помощью запроса на выборку, 
+который включает несколько таблиц:
+Задание
+Включить новых авторов в таблицу author с помощью запроса на добавление,
+  а затем вывести все данные из таблицы author. 
+Новыми считаются авторы, которые есть в таблице supply, но нет в таблице author.
+  INSERT INTO author
+SELECT  name_author,supply.author
+FROM 
+    author 
+    RIGHT JOIN supply on author.name_author = supply.author
+WHERE name_author IS Null;
+SELECT * FROM author;
