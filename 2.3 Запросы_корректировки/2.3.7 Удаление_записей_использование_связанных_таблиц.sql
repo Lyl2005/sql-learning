62d740c7533224b704aa1f16f432136c9f95bf36
Lyl2005 <lyl_2005@rambler.ru>
2024-12-19 19:47:43 +0300

    Update 2.3.7 Удаление_записей_использование_связанных_таблиц.sql

--- 2.3 Запросы_корректировки/2.3.7 Удаление_записей_использование_связанных_таблиц.sql
+++ 2.3 Запросы_корректировки/2.3.7 Удаление_записей_использование_связанных_таблиц.sql
@@ -9,7 +9,12 @@ WHERE ...
 Пример
 Удалить всех авторов из таблицы author, у которых есть книги, количество экземпляров которых меньше 3. 
 Из таблицы book удалить все книги этих авторов.
-Запрос:
+DELETE FROM author
+WHERE author_id IN 
+(SELECT author_id FROM book
+WHERE AMOUNT<3);
+SELECT * FROM book
+или
 DELETE FROM author
 USING 
     author 
@@ -17,3 +22,16 @@ USING
 WHERE book.amount < 3;
 SELECT * FROM author;
 SELECT * FROM book;
+
+Задание
+Удалить всех авторов, которые пишут в жанре "Поэзия". 
+Из таблицы book удалить все книги этих авторов. 
+В запросе для отбора авторов использовать полное название жанра, а не его id.
+DELETE FROM author
+USING author
+INNER JOIN book ON author.author_id=book.author_id
+INNER JOIN genre ON genre.genre_id=book.genre_id
+WHERE name_genre = "Поэзия";
+SELECT * FROM book;
+SELECT * FROM author
+    
